fb7cf44af1fd021e944f7fc40a1d6b570054cc45
Sargent-Neon <lruss6@ocdsb.ca>
2024-06-19 13:53:28 -0400

    Add files via upload

---
+++ Cutscenes/Text.js
@@ -0,0 +1,23 @@
+var line = 0;
+var lines = [];
+
+async function loadFileAndPrintToConsole(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
+    lines = data.split("\n");
+  } catch (err) {
+    console.error(err);
+  }
+  nextScene(" ");
+}
+
+function nextScene(location){
+  if(line == lines.length){
+    window.location.href = "/"+location;
+  }
+  else{
+    document.getElementById("text").innerHTML = lines[line];
+    line++;
+  }
+}
