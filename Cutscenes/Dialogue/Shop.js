ce57b0bd7f8b4b2df9d4782f2cac0b28e59d3b6d
Sargent-Neon <lruss6@ocdsb.ca>
2024-06-19 13:47:17 -0400

    Add files via upload

---
+++ Cutscenes/Dialogue/Shop.js
@@ -0,0 +1,50 @@
+var line = 0;
+var lines = [];
+
+async function loadFileAndPrintToConsole(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
+    lines = data.split("\n");
+  } catch (err) {
+    console.error(err);
+  }
+  nextScene(" ");
+}
+
+function nextScene(location){
+  if(line == lines.length){
+    window.location.href = "/"+location;
+  }
+  var op = document.getElementById("NPC");
+  op.style.opacity = 1;
+  op = document.getElementById("Player");
+  op.style.opacity = 1;
+  var buffer = lines[line].split(": ");
+  if (buffer[0].indexOf("You") == 0 || buffer[0].indexOf("Shopper") == 0 || buffer[0].indexOf("Shopkeeper") == 0){
+    document.getElementById("text").innerHTML = buffer[1];
+    line++;
+    if(buffer[0].indexOf("You") == 0){
+      var char = document.getElementById("NPC")
+      char.style.opacity = 0.5;
+    }
+    else{
+      var char = document.getElementById("Player")
+      char.style.opacity = 0.5;
+      if(buffer[0].indexOf("Shopper") == 0){
+        document.getElementById("NPC").src = "/NCPs/NPC6_Shopper.png"
+      }
+      else{
+        document.getElementById("NPC").src = "/NCPs/NPC5_ShopClerk.png"
+      }
+    }
+  }
+  else{
+    document.getElementById("text").innerHTML = buffer[0];
+    line++;
+    var char = document.getElementById("NPC")
+    char.style.opacity = 0.5;
+    char = document.getElementById("Player")
+    char.style.opacity = 0.5;
+  }
+}
